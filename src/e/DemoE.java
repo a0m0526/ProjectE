2bcd5bde72b9237636a955c7042a7009d8381464
Ross <Ross@192.168.1.237>
2024-01-03 20:02:44 -0600

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,12 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo E");
+		
+		System.out.println("Yeah1");
+		System.out.println("yeah2");
+	}
+
+}
